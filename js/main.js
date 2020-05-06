3a2931e74e35fff73680b6069abefde397e29dc8
ipeleng-bela <ipeleng.bela@gmail.com>
2020-05-06 14:34:44 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 // API key
-const key = '$2a$10$1JEnmtEF417yBaFZcr51qukRjaKv8d5toEG5DKP/IUZWIVwfsaF7y';
+const key = '$2a$10$1JEnmtEF417yBaFZcr51qukRjaKv8d5toEG5DKP/IUZWIVwfsaF7y';co 
+
+// Characters list
+const ul = document.getElementById('characters'):
